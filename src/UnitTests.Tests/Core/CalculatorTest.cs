ac2413721a5b003f837dad031f0703d79d46bd24
gcishevs <german.cishevskiy@gmail.com>
2013-01-27 20:50:01 +0200

    initial commit

---
+++ src/UnitTests.Tests/Core/CalculatorTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Core;
+
+namespace UnitTests.Tests.Core
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Check_Add_Without_Parameters()
+        {
+            Calculator calc = new Calculator();
+            var actual = calc.Add();
+            Assert.AreEqual(actual, 0);
+        }
+
+        [TestMethod]
+        public void Check_Add_Without_One_Parameter()
+        {
+            
+            //arange
+            int parameter = 10;
+            var expected = parameter;
+
+            //act
+             Calculator calc = new Calculator();
+            int actual = calc.Add(parameter);
+
+            //asert
+            
+            Assert.AreEqual(expected, actual);
+          
+        }
+    }
+}
