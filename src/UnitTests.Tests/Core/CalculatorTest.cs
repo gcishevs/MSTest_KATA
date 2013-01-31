0983da76917fbeff2a82ddf83639bcdce35cbd93
gcishevs <german.cishevskiy@gmail.com>
2013-01-31 21:47:30 +0200

    31.01 15 minutes kata n1

--- src/UnitTests.Tests/Core/CalculatorTest.cs
+++ src/UnitTests.Tests/Core/CalculatorTest.cs
@@ -6,67 +6,105 @@ namespace UnitTests.Tests.Core
     [TestClass]
     public class CalculatorTest
     {
-        [TestMethod]
-        public void Test_Add_With_Empty_String()
-        {
-            //arrange
-            int expected = 0;
-
-            //act
-            Calculator calc = new Calculator();
-            var actual = calc.Add("");
-
-            //assert
-            Assert.AreEqual(actual, expected);
-        }
 
         [TestMethod]
-        public void Test_Add_With_One_Parameter()
+        public void CalculatorTest_Check_If_EmptyString_Return_Zerro()
         {
-            
-            //arrange
-            int parameter = 10;
-            var expected = parameter;
-
-            //act
-            Calculator calc = new Calculator();
-            int actual = calc.Add("10");
-
-            //asert
-            Assert.AreEqual(expected, actual);
-          
-        }
-
-        [TestMethod]
-        public void Test_Add_With_Two_Parameters()
-        {
-            //arrange
-            int firstSummand = 23;
-            int secondSummand = 32;
-            int expected = 55;
-
-            //act
-            Calculator calc = new Calculator();
-            var actual = calc.Add("23,32");
-
-            //assert
-
-            Assert.AreEqual(expected, actual);
-
-        }
-
-        [TestMethod]
-        public void Test_Add_With_Incorrect_Delimeter()
-        {
-            //arrange
+            //Arrange
+            var calc = new Calculator();
             var expected = 0;
-
-            //act
-            Calculator calc = new Calculator();
-            var actual = calc.Add("23:45");
-
-            //assert
+            //Act
+            var actual = calc.Add("");
+            //Actual
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CalculatorTest_CheckIfOneParameterReturnsTheParameterValue()
+        {
+            //Arrange
+            var calc = new Calculator();
+            var expected = 167;
+            //Act
+            var actual = calc.Add("167");
+            //Actual
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        // Первые тесты, не подглядывать!!!!!!!!!!!!!
+
+        //[TestMethod]
+        //public void Test_Add_With_Empty_String()
+        //{
+        //    //arrange
+        //    int expected = 0;
+
+        //    //act
+        //    Calculator calc = new Calculator();
+        //    var actual = calc.Add("");
+
+        //    //assert
+        //    Assert.AreEqual(actual, expected);
+        //}
+
+        //[TestMethod]
+        //public void Test_Add_With_One_Parameter()
+        //{
+            
+        //    //arrange
+        //    int parameter = 10;
+        //    var expected = parameter;
+
+        //    //act
+        //    Calculator calc = new Calculator();
+        //    int actual = calc.Add("10");
+
+        //    //asert
+        //    Assert.AreEqual(expected, actual);
+          
+        //}
+
+        //[TestMethod]
+        //public void Test_Add_With_Two_Parameters()
+        //{
+        //    //arrange
+        //    int firstSummand = 23;
+        //    int secondSummand = 32;
+        //    int expected = 55;
+
+        //    //act
+        //    Calculator calc = new Calculator();
+        //    var actual = calc.Add("23,32");
+
+        //    //assert
+
+        //    Assert.AreEqual(expected, actual);
+
+        //}
+
+        //[TestMethod]
+        //public void Test_Add_With_Incorrect_Delimeter()
+        //{
+        //    //arrange
+        //    var expected = 0;
+
+        //    //act
+        //    Calculator calc = new Calculator();
+        //    var actual = calc.Add("23:45");
+
+        //    //assert
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
--- src/UnitTests/Core/Calculator.cs
+++ src/UnitTests/Core/Calculator.cs
@@ -4,28 +4,56 @@ namespace UnitTests.Core
     public class Calculator
     {
 
-        public int Add(string numbers)
+
+
+        public int Add(string parameters)
         {
-            if (string.IsNullOrEmpty(numbers))
-            {
+            if (string.IsNullOrEmpty(parameters))
                 return 0;
-            }
-
-            string[] terms = numbers.Split(',');
-            if (terms.Length == 1)
+            else
             {
-                int parameter = 0;
-                int.TryParse(terms[0], out parameter);
-                return parameter;
+                string[] summands = parameters.Split(',');
+                if (summands.Length == 1)
+                {
+                    int summand = 0;
+                    int.TryParse(summands[0], out summand);
+                    return summand;
+                }
             }
-            else if (terms.Length == 2)
-            {
-                int firstSummand = int.Parse(terms[0]);
-                int secondSummand = int.Parse(terms[1]);
-                return firstSummand + secondSummand;
-            }
-
             return 0;
         }
+
+
+
+
+
+
+
+
+        // первый раз, как в первый класс
+
+        //public int Add(string numbers)
+        //{
+        //    if (string.IsNullOrEmpty(numbers))
+        //    {
+        //        return 0;
+        //    }
+
+        //    string[] terms = numbers.Split(',');
+        //    if (terms.Length == 1)
+        //    {
+        //        int parameter = 0;
+        //        int.TryParse(terms[0], out parameter);
+        //        return parameter;
+        //    }
+        //    else if (terms.Length == 2)
+        //    {
+        //        int firstSummand = int.Parse(terms[0]);
+        //        int secondSummand = int.Parse(terms[1]);
+        //        return firstSummand + secondSummand;
+        //    }
+
+        //    return 0;
+        //}
     }
 }
